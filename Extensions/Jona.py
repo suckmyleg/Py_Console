89c4775f25e4d114f2266eddadb4ba9579d99e5f
suckmyleg <juanito-146@hotmail.es>
2022-12-11 11:07:38 +0100

    Saving

---
+++ Extensions/Jona.py
@@ -0,0 +1,12 @@
+from time import sleep
+
+def start(t, c, m):
+
+	def jona(*args):
+		print("Mi polla con corona")
+
+	def testFun(*args):
+		try:
+			t.trigger_command
+
+	t.add_command("jona", jona)
